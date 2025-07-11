64ae5f8c2ba018ead0482bfcc354c63f63e20301
Boris Brock <gabrielborisbrock@gmail.com>
2025-07-11 09:47:34 +0200

    Add device enumeration functions using miniaudio (#7)

--- client/libvoice/voice.h
+++ client/libvoice/voice.h
@@ -1,13 +1,21 @@
 #pragma once
 
+#include <vector>
+#include <string>
+
 namespace voice {
 void dummy();
+std::vector<std::string> get_output_devices();
+std::vector<std::string> get_capture_devices();
 }
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 void voice_dummy();
+char** voice_get_output_devices(int* count);
+char** voice_get_capture_devices(int* count);
+void voice_free_device_list(char** list, int count);
 #ifdef __cplusplus
 }
 #endif
