1cd5af8818781fe3f83608a10475b86838107ff1
Boris Brock <gabrielborisbrock@gmail.com>
2025-07-12 06:07:14 +0200

    Refactoring

--- client/libchat/chat.cpp
+++ client/libchat/chat.cpp
@@ -8,86 +8,91 @@
 #include <cstring>
 #include <thread>
 
-namespace chat {
-
-constexpr const char* PUSH_ADDR = "tcp://127.0.0.1:6001";
-constexpr const char* SUB_ADDR  = "tcp://127.0.0.1:6002";
-
-static nng_socket pushSock;
-static nng_socket subSock;
-static std::thread recvThread;
-static std::atomic<bool> running{false};
-static message_callback callback = nullptr;
-
-static void recv_loop()
+namespace chat
 {
-    while (running) {
-        char* msg = nullptr;
-        size_t size = 0;
-        int rv = nng_recv(subSock, &msg, &size, NNG_FLAG_ALLOC);
-        if (rv == 0) {
-            if (callback)
-                callback(msg);
-            nng_free(msg, size);
-        } else if (rv == NNG_ETIMEDOUT) {
-            continue;
+
+    constexpr const char *PUSH_ADDR = "tcp://127.0.0.1:6001";
+    constexpr const char *SUB_ADDR = "tcp://127.0.0.1:6002";
+
+    static nng_socket pushSock;
+    static nng_socket subSock;
+    static std::thread recvThread;
+    static std::atomic<bool> running{false};
+    static message_callback callback = nullptr;
+
+    static void recv_loop()
+    {
+        while (running)
+        {
+            char *msg = nullptr;
+            size_t size = 0;
+            int rv = nng_recv(subSock, &msg, &size, NNG_FLAG_ALLOC);
+            if (rv == 0)
+            {
+                if (callback)
+                    callback(msg);
+                nng_free(msg, size);
+            }
+            else if (rv == NNG_ETIMEDOUT)
+            {
+                continue;
+            }
         }
     }
-}
 
-bool start(message_callback cb)
-{
-    callback = cb;
-    int rv;
-    if ((rv = nng_push0_open(&pushSock)) != 0)
-        return false;
-    if ((rv = nng_sub0_open(&subSock)) != 0)
-        return false;
-    if ((rv = nng_setopt(subSock, NNG_OPT_SUB_SUBSCRIBE, "", 0)) != 0)
-        return false;
-    if ((rv = nng_dial(pushSock, PUSH_ADDR, nullptr, 0)) != 0)
-        return false;
-    if ((rv = nng_dial(subSock, SUB_ADDR, nullptr, 0)) != 0)
-        return false;
+    bool start(message_callback cb)
+    {
+        callback = cb;
+        int rv;
+        if ((rv = nng_push0_open(&pushSock)) != 0)
+            return false;
+        if ((rv = nng_sub0_open(&subSock)) != 0)
+            return false;
+        if ((rv = nng_setopt(subSock, NNG_OPT_SUB_SUBSCRIBE, "", 0)) != 0)
+            return false;
+        if ((rv = nng_dial(pushSock, PUSH_ADDR, nullptr, 0)) != 0)
+            return false;
+        if ((rv = nng_dial(subSock, SUB_ADDR, nullptr, 0)) != 0)
+            return false;
 
-    running = true;
-    recvThread = std::thread(recv_loop);
-    return true;
-}
+        running = true;
+        recvThread = std::thread(recv_loop);
+        return true;
+    }
 
-void publish(const char* message)
-{
-    if (!message)
-        return;
-    nng_send(pushSock, (void*)message, std::strlen(message) + 1, 0);
-}
+    void publish(const char *message)
+    {
+        if (!message)
+            return;
+        nng_send(pushSock, (void *)message, std::strlen(message) + 1, 0);
+    }
 
-void stop()
-{
-    running = false;
-    nng_close(pushSock);
-    nng_close(subSock);
-    if (recvThread.joinable())
-        recvThread.join();
-}
+    void stop()
+    {
+        running = false;
+        nng_close(pushSock);
+        nng_close(subSock);
+        if (recvThread.joinable())
+            recvThread.join();
+    }
 
 } // namespace chat
 
-extern "C" {
-
-bool chat_start(chat_message_callback cb)
+extern "C"
 {
-    return chat::start(cb);
-}
 
-void chat_publish(const char* message)
-{
-    chat::publish(message);
-}
+    bool chat_start(chat_message_callback cb)
+    {
+        return chat::start(cb);
+    }
 
-void chat_stop()
-{
-    chat::stop();
-}
+    void chat_publish(const char *message)
+    {
+        chat::publish(message);
+    }
 
+    void chat_stop()
+    {
+        chat::stop();
+    }
 }
--- client/libchat/chat.h
+++ client/libchat/chat.h
@@ -2,23 +2,25 @@
 
 #include <stddef.h>
 
-namespace chat {
-using message_callback = void(*)(const char*);
+namespace chat
+{
+    using message_callback = void (*)(const char *);
 
-bool start(message_callback cb);
-void publish(const char* message);
-void stop();
+    bool start(message_callback cb);
+    void publish(const char *message);
+    void stop();
 }
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-typedef void(*chat_message_callback)(const char*);
+    typedef void (*chat_message_callback)(const char *);
 
-bool chat_start(chat_message_callback cb);
-void chat_publish(const char* message);
-void chat_stop();
+    bool chat_start(chat_message_callback cb);
+    void chat_publish(const char *message);
+    void chat_stop();
 
 #ifdef __cplusplus
 }
